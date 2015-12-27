3d10963265e27a1dd770c0c8313da74ca07175d0
TheConnMan <bcconn2112@gmail.com>
2015-12-27 14:45:52 -0500

    Change views to use ngRoute

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,51 @@
-angular.module('chat', ['btford.socket-io']).
-factory('socket', function(socketFactory) {
+angular.module('chat', ['ngRoute', 'btford.socket-io'])
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: '/partials/username.html'
+	})
+	.when('/chat', {
+		templateUrl: '/partials/chat.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}])
+.factory('socket', function(socketFactory) {
 	return socketFactory();
-}).controller('Chat', ['$scope', '$http', 'socket', function($scope, $http, socket) {
-	$scope.username = '';
+})
+.controller('Chat', ['$scope', '$http', '$location', 'socket', function($scope, $http, $location, socket) {
 	$scope.messages = [];
+	$scope.user = {
+		username: ''
+	};
+	$scope.chat = {
+		message: ''
+	};
 
 	socket.on('send:message', function(message) {
 		$scope.messages.push(message);
 	});
 
 	$scope.sendUsername = function() {
-		if ($scope.username.length !== 0) {
-			socket.emit('username', $scope.username);
-			$scope.init = true;
+		if ($scope.user.username.length !== 0) {
+			socket.emit('username', $scope.user.username);
+			$location.path('/chat');
+		}
+	};
+
+	$scope.sendMessage = function() {
+		if ($scope.chat.message.length !== 0) {
+			socket.emit('message', $scope.chat.message);
+			$scope.chat.message = '';
 			setTimeout(function() {
 				$('#message').focus();
 			}, 0);
 		}
 	};
 
-	$scope.sendMessage = function() {
-		if ($scope.message.length !== 0) {
-			socket.emit('message', $scope.message);
-			$scope.message = '';
-		}
-	};
-
 	$scope.differentUser = function(index) {
 		return $scope.messages[index].user != $scope.messages[index - 1].user;
 	};
-
-	$('#username').focus();
 }]).directive('enter', function() {
 	return function($scope, $element, $attrs) {
 		$element.bind('keydown keypress', function(e) {
