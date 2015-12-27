a43a8fa7ed1893346a1a316dc8b93a14f2b39fec
TheConnMan <bcconn2112@gmail.com>
2015-12-26 22:14:53 -0500

    Emit message on user join

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var moment = require('moment');
+
 io.on('connection', function() {
 	console.log('New connection');
 });
@@ -10,6 +12,11 @@ io.on('connection', function() {
 app.use(express.static('public'));
 
 app.get('/api/username', function(req, res) {
+	io.sockets.emit('send:message', {
+		text: req.query.username + ' joined',
+		date: moment().format('HH:mm ss'),
+		type: 'join'
+	});
 	res.json({
 		ok: true
 	});
